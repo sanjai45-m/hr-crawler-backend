10ecf2d020a21ec6dd84d835abc3c198ca8e81f2
sanjai45-m <sanjaim202@gmail.com>
2025-07-07 18:41:46 +0530

    firrt

--- server.js
+++ server.js
@@ -127,6 +127,8 @@ async function crawlNaukri(role, location, experience = '') {
         console.log(`Scraping Naukri for ${role} in ${location}...`);
         browser = await puppeteer.launch({
             headless: false, // Set to true in production
+              executablePath: 'C:\\Users\\Abcom\\.cache\\puppeteer\\chrome\\win64-138.0.7204.92\\chrome-win64\\chrome.exe',
+
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
 
